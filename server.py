70f926b18a31f6096462cab2689ee00ce7cdb7dd
p4kner <p4kner@gmail.com>
2018-01-15 16:03:06 +0100

    Reading out and modifying contents in scraper_db.

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from flask.ext.jsonpify import jsonify
 
 
 # Connect to the database.
-db_connect = MySQLdb.connect(host="localhost",
+db = MySQLdb.connect(host="localhost",
     user="root",
     passwd="pw",
     db="scraper_db",
@@ -24,28 +24,28 @@ api = Api(app)
 # Return all contents.
 class Contents(Resource):
     def get(self):
-        conn = db_connect.cursor()
-        query = conn.execute('SELECT * FROM contents')
-        return {'contents': [i[0] for i in query.cursor.fetchall()]}
+        cursor = db.cursor()
+        query = cursor.execute('SELECT * FROM contents')
+        data = cursor.fetchall()
+        return jsonify(data)
 
-
+# Specific content item by id.
 class Contents_Id(Resource):
     def get(self, content_id):
-        conn = db_connect.cursor()
-        query = conn.execute('SELECT * FROM contents WHERE id =%d ' % int(content_id))
-        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
-        return jsonify(result)
+        cursor = db.cursor()
+        query = cursor.execute('SELECT * FROM contents WHERE id =%d ' % int(content_id))
+        data = cursor.fetchall()
+        return jsonify(data)
 
 # Add data to db.
 class Submit(Resource):
     def get(self):
         return 'get request was sent - no entry was made. Please use post.'
     def post(self):
-        conn = db_connect.cursor()
-        query = conn.execute('INSERT INTO contents(title, contents) VALUES (\'Super Headline\', \'This is some quality content here.\')')
+        cursor = db.cursor()
+        query = cursor.execute('INSERT INTO contents(title, contents) VALUES (\'Super Headline\', \'This is some quality content here.\')')
+        db.commit()
         return 'POST REQUEST WAS SENT - database entry was added.'
-
-
 
 
 api.add_resource(Contents, '/contents')
