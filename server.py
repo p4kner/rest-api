360a04fe9acd7ce6666422b4a5a4bdc7ef4cf161
p4kner <p4kner@gmail.com>
2018-01-15 15:38:37 +0100

    Removal of sqlite. Replaced with MySQL.

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
+import MySQLdb
 from json import dumps
 from flask.ext.jsonpify import jsonify
 
 
+# Connect to the database.
+db_connect = MySQLdb.connect(host="localhost",
+    user="root",
+    passwd="pw",
+    db="scraper_db",
+    charset="utf8",
+    use_unicode=True,)
 
-db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
 
@@ -15,43 +21,36 @@ api = Api(app)
 # Connection to database is established, query is executed and (jsonified)
 # results are returned.
 
-class Employees(Resource):
+# Return all contents.
+class Contents(Resource):
     def get(self):
-        conn = db_connect.connect()
-        query = conn.execute('SELECT * FROM employees')
-        return {'employees': [i[0] for i in query.cursor.fetchall()]}
+        conn = db_connect.cursor()
+        query = conn.execute('SELECT * FROM contents')
+        return {'contents': [i[0] for i in query.cursor.fetchall()]}
 
 
-class Tracks(Resource):
-    def get(self):
-        conn = db_connect.connect()
-        query = conn.execute('SELECT trackid, name, composer, unitprice FROM tracks;')
-        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
-        return jsonify(result)
-
-
-class Employees_Name(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute('SELECT * FROM employees WHERE EmployeeId =%d ' % int(employee_id))
+class Contents_Id(Resource):
+    def get(self, content_id):
+        conn = db_connect.cursor()
+        query = conn.execute('SELECT * FROM contents WHERE id =%d ' % int(content_id))
         result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
         return jsonify(result)
 
 # Add data to db.
-class Newbie(Resource):
+class Submit(Resource):
     def get(self):
         return 'get request was sent - no entry was made. Please use post.'
     def post(self):
-        conn = db_connect.connect()
-        query = conn.execute('INSERT INTO employees(FirstName, LastName, Title) VALUES (\'Jason\', \'DeRulo\', \'Producer\')')
+        conn = db_connect.cursor()
+        query = conn.execute('INSERT INTO contents(title, contents) VALUES (\'Super Headline\', \'This is some quality content here.\')')
         return 'POST REQUEST WAS SENT - database entry was added.'
 
 
 
-api.add_resource(Employees, '/employees')
-api.add_resource(Tracks, '/tracks')
-api.add_resource(Employees_Name, '/employees/<employee_id>')
-api.add_resource(Newbie, '/newbie') # Post to flask.
+
+api.add_resource(Contents, '/contents')
+api.add_resource(Contents_Id, '/contents/<content_id>')
+api.add_resource(Submit, '/submit') # Post to flask.
 
 if __name__ == '__main__':
     app.run(port=5002)
